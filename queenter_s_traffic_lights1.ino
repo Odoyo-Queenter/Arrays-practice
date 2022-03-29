1b4fb91df76ead84d8b28fa0755e9258dfa21396
Odoyo-Queenter <99411475+Odoyo-Queenter@users.noreply.github.com>
2022-03-29 21:07:40 +0300

    Add files via upload

---
+++ queenter_s_traffic_lights1.ino
@@ -0,0 +1,56 @@
+// c++
+//
+int red = 12;
+int red2 = 3;
+int yellow = 11;
+int yellow2 =2;
+int green =10;
+int green2 = 1;
+void setup()
+{
+  pinMode(red, OUTPUT);
+  pinMode(red2, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(yellow2, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(green2, OUTPUT);
+}
+
+
+
+void loop()
+{
+digitalWrite(red,HIGH);
+delay(1000);
+digitalWrite(green2, HIGH);
+delay(1000);
+digitalWrite(red, LOW);
+delay(1000);
+digitalWrite(green2, LOW);
+delay(1000);
+digitalWrite(yellow, HIGH);
+delay(1000);
+digitalWrite(yellow2, HIGH);
+delay(1000);
+digitalWrite(yellow, LOW);
+delay(500);
+digitalWrite(yellow2, LOW);
+delay(500);
+digitalWrite(red2,HIGH);
+delay(1000); 
+digitalWrite(green, HIGH);
+delay(1000); 
+digitalWrite(red2, LOW);
+delay(1000);
+digitalWrite(green, LOW);
+delay(1000);
+digitalWrite(yellow, HIGH);
+delay(1000);
+digitalWrite(yellow2, HIGH);
+delay(1000);
+ digitalWrite(yellow, LOW);
+delay(500);
+digitalWrite(yellow2, LOW);
+delay(500); 
+  
+}  
